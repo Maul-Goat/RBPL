8cbdd4a29633727499b663fea0622e0dba21e744
Maul-Goat <achmadmaulanaxx@gmail.com>
2026-04-19 18:21:58 +0700

    Update tagihan.php

--- AppRs/tagihan.php
+++ AppRs/tagihan.php
@@ -18,6 +18,7 @@ $query_tagihan = mysqli_query($koneksi, "
 <html lang="id">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Riwayat & Tagihan</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
